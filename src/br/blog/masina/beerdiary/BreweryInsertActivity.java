856903d2290ecd44b95dc97cc72c0e1a69c7f3a5
Daniel Merino Masina <daniel.masina@gmail.com>
2011-11-13 12:47:51 -0200

    Brewery data validation

--- src/br/blog/masina/beerdiary/BreweryInsertActivity.java
+++ src/br/blog/masina/beerdiary/BreweryInsertActivity.java
@@ -2,7 +2,6 @@ package br.blog.masina.beerdiary;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
